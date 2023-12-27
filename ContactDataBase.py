98fe76823f33b09d7bfa33b589be7c3ab2869933
Abolfazl Montazeri <abolfazle.m82@gmail.com>
2023-12-28 01:54:29 +0330

    Edit minor bug

--- ContactDataBase.py
+++ ContactDataBase.py
@@ -34,8 +34,8 @@ class ContactDataBase:
                 contact.last_name = contact.last_name.replace("'", "\'")
 
             insert_query = f'''INSERT INTO CONTACTDATA (first_name, last_name, phone_number)
-             VALUES ('{contact.first_name}','{contact.last_name}', '{contact.phone_number}')'''
-            cursor.execute(insert_query)
+                         VALUES (?,?,?)'''
+            cursor.execute(insert_query, (contact.first_name, contact.last_name, contact.phone_number))
             connection.commit()
             cursor.close()
 
@@ -43,7 +43,7 @@ class ContactDataBase:
         with sqlite3.connect(database_file,
                              detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
             cursor = connection.cursor()
-            select_query = f'''SELECT * FROM CONTACTDATA;'''
+            select_query = f'''SELECT * FROM CONTACTDATA ORDER BY first_name ASC, last_name ASC;'''
             result = cursor.execute(select_query).fetchall()
             connection.commit()
             cursor.close()
@@ -54,18 +54,16 @@ class ContactDataBase:
                                         last_name=row[2],
                                         phone_number=row[3],
                                         created_time=row[4]))
-        return contact_list if len(contact_list) > 0 else None
+        return contact_list
 
     def check_repeated_number(self, phone_number: str) -> bool:
         with sqlite3.connect(database_file,
                              detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
             cursor = connection.cursor()
             select_query = f'''select count(*) from CONTACTDATA where phone_number = '{phone_number}';'''
-            print(select_query)
             result = cursor.execute(select_query).fetchall()
             connection.commit()
             cursor.close()
-        print(result)
         if result[0][0] == 0:
             return True
         return False
