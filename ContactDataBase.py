82cd80bac444feb895517ac3ba48787550a86b05
Abolfazl Montazeri <abolfazle.m82@gmail.com>
2023-12-28 00:02:47 +0330

    Add edit menu functionality

--- ContactDataBase.py
+++ ContactDataBase.py
@@ -69,3 +69,43 @@ class ContactDataBase:
         if result[0][0] == 0:
             return True
         return False
+
+    def search(self, search_input: str) -> list[Contact] or None:
+        with sqlite3.connect(database_file,
+                             detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
+            cursor = connection.cursor()
+            if search_input.isnumeric():
+                select_query = f'''SELECT * FROM CONTACTDATA WHERE phone_number like '%{search_input}%';'''
+                result = cursor.execute(select_query).fetchall()
+                connection.commit()
+                cursor.close()
+            else:
+                select_query = f'''SELECT * FROM CONTACTDATA;'''
+                all = cursor.execute(select_query).fetchall()
+                connection.commit()
+                cursor.close()
+                result = []
+                for row in all:
+                    if search_input in row[1] + row[2]:
+                        result.append(row)
+
+        contact_list = []
+        for row in result:
+            contact_list.append(Contact(database_id=row[0],
+                                        first_name=row[1],
+                                        last_name=row[2],
+                                        phone_number=row[3],
+                                        created_time=row[4]))
+        return contact_list if len(contact_list) > 0 else None
+
+    def edit(self, contact: Contact):
+        with sqlite3.connect(database_file,
+                             detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as connection:
+            cursor = connection.cursor()
+            query = f'''UPDATE CONTACTDATA
+                               SET first_name = '{contact.first_name}', last_name = '{contact.last_name}',
+                                phone_number = '{contact.phone_number}' 
+                                WHERE id = {contact.database_id} ;'''
+            result = cursor.execute(query)
+            connection.commit()
+            cursor.close()
--- Menu.py
+++ Menu.py
@@ -1,9 +1,14 @@
 from textwrap import dedent
 from Contact import Contact
 from ContactDataBase import ContactDataBase
+from rich.console import Console
+from rich.table import Table
+
+
 class Menu:
     def __init__(self, database: ContactDataBase):
         self.database = database
+        self.console = Console()
         self.main_menu()
 
     def main_menu(self):
@@ -12,10 +17,8 @@ class Menu:
             What do you want to do with you contacts?
             1- Add a new one
             2- See all of them
-            3- Search in all of them
-            4- Delete
-            5- Edit
-            6- Exit
+            3- Find, edit or delete
+            4- Exit
             ''')))
             match command:
                 case 1:
@@ -25,10 +28,6 @@ class Menu:
                 case 3:
                     self.search()
                 case 4:
-                    self.delete()
-                case 5:
-                    self.edit()
-                case 6:
                     break
                 case _:
                     print('Undefined command, try again!')
@@ -50,7 +49,8 @@ class Menu:
                     print(str(e))
             while True:
                 try:
-                    contact.last_name = input("Enter your contact's lastname (This is optional, if you don't want to add this field just press enter) : ")
+                    contact.last_name = input(
+                        "Enter your contact's lastname (This is optional, if you don't want to add this field just press enter) : ")
                     break
                 except ValueError as e:
                     print(str(e))
@@ -66,11 +66,96 @@ class Menu:
         pass
 
     def search(self):
-        pass
+        while True:
+            search_input = input(
+                'Enter part of name or phone number of the contact you want to find (or type \"return\" to get back): ')
+            if search_input == 'return':
+                break
+            res = self.database.search(search_input)
+            table = Table("number", "Name", "Phone Number", "Date Added", title="Found Contacts: ")
+            if res is not None:
+                res = list(enumerate(res, start=1))
+                for contact in res:
+                    table.add_row(str(contact[0]),
+                                  contact[1].first_name,
+                                  contact[1].phone_number,
+                                  str(contact[1].created_time))
+                self.console.print(table)
+                break_outer_loop = False
+                while True:
+                    after_search_command = int(input(dedent('''
+                            What do you want to do with these found contacts?
+                            1- Edit one of them
+                            2- Delete some of them
+                            3- Delete all of them
+                            4- Search again
+                            5- Do nothing and return
+                            ''')))
+                    match after_search_command:
+                        case 1:
+                            while True:
+                                selected_contact_number = int(input('Enter number of the contact you want to edit: '))
+                                if 1 <= selected_contact_number <= len(res):
+                                    break
+                                else:
+                                    print('Enter a number that exists on the result table!')
+                            self.edit(res[selected_contact_number - 1][1])
+                        case 2:
+                            pass
+                        case 3:
+                            pass
+                        case 4:
+                            break
+                        case 5:
+                            break_outer_loop = True
+                            break
+                        case _:
+                            print('Undefined command, try again!')
+                if break_outer_loop:
+                    break
+            else:
+                print("Nothing found!")
+                continue
 
     def delete(self):
         pass
 
-    def edit(self):
-        pass
-
+    def edit(self, contact: Contact):
+        while True:
+            edit_command = int(input(dedent('''
+                Which one of the fields do you wish to edit?:
+                1- First Name
+                2- Last Name
+                3- Phone Number
+                4- Save and get back
+                5- Discard the changes and get back
+                ''')))
+            match edit_command:
+                case 1:
+                    while True:
+                        try:
+                            contact.first_name = input("Enter your contact's new first name : ")
+                            break
+                        except ValueError as e:
+                            print(str(e))
+                case 2:
+                    while True:
+                        try:
+                            contact.last_name = input("Enter your contact's new last name : ")
+                            break
+                        except ValueError as e:
+                            print(str(e))
+                case 3:
+                    while True:
+                        try:
+                            contact.phone_number = input("Enter your contact's new phone number : ")
+                            break
+                        except ValueError as e:
+                            print(str(e))
+                case 4:
+                    self.database.edit(contact)
+                    break
+                case 5:
+                    break
+                case _:
+                    print('Undefined command, try again!')
